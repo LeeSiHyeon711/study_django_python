2ad93931ae52d2eebc3b34937bc85a9ecb8478e4
Si Hyeon Lee <dltlgus1230@naver.com>
2025-12-20 13:53:26 +0900

    [feat] add_create_and_update_and_delete

--- ch01/blog/views.py
+++ ch01/blog/views.py
@@ -8,8 +8,11 @@ from django.views.generic import FormView
 from blog.forms import PostSearchForm
 from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
 
-# Create your view here.
 
 #--- ListView
 class PostLV(ListView):
@@ -44,7 +47,7 @@ class PostDAV(DayArchiveView):
 
 class PostTAV(TodayArchiveView):
     model = Post
-    date_fiels = 'modify_dt'
+    date_field = 'modify_dt'
 
 #--- TagView
 class TagCloudTV(TemplateView):
@@ -88,3 +91,29 @@ class SearchFormView(FormView):
         context['search_term'] = searchWord
         context['object_list'] = post_list
         return render(self.request, self.template_name, context)  # No Redirection
+
+#--- PostCreateView
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'tags', 'description', 'content']
+    initial = {'slug': 'auto-filling-do-not-input'}
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+class PostUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tags']
+    success_url = reverse_lazy('blog:index')
+
+class PostDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
--- ch01/bookmark/models.py
+++ ch01/bookmark/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
     title = models.CharField('TITLE', max_length=100, blank=True)  # 글자수 100자 제한, 공백 허용
     url = models.URLField('URL', unique=True)  # 유일한 URL 필드값 허용
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
